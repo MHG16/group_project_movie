ee0e3e78889100a09556cf7cebfbbc7ce00f1f1a
Martin Grossmann <mhgrossmann@yahoo.com>
2016-04-09 08:06:37 -0500

    MovieView started

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -1,7 +1,8 @@
 // import the stylesheet. this is necessary so that webpack will compile all the sass into css and then build it into our style.css file
 import './../styles/main.scss';
 
-//import views and collections here
+import $ from 'jquery';
+import Backbone from 'backbone'
 
 
 //instantiate a movie view  
@@ -12,21 +13,42 @@ let updatedMovieList = new MovieView();
 
 $('body').append(updatedMovieList.$el);
 
-//get the latest movie information from the server when page loads  
-var settings = {
-	success: function() {
-		movieCollection.forEach((todo) => {
-			console.log(todo.get('text'));
-			//instantiate each Todo Item View
-			let newPersonalTodoListView = new TodoItemView(
-				todo.get('text')
-			);
-			//append the 
-			$('.todo-list1').append(newPersonalTodoListView.el);
-		});
+const MovieView = Backbone.View.extend ({
+	tagName: 'section',  //section is the root element for the TodoListView
+	
+	initialize: function() {
+		this.render();
+	},
+	render: function() {  //code to put the elements needed into the root element
+		const template = `
+		<p>Hello World</p>`; 
+
+		this.$el.html(template);  //html here replaces anything in the section with the template.
+
 	}
-};
-updatedMovieCollection.fetch(settings);
+	
+});
+
+
+
+
+
+
+//get the latest movie information from the server when page loads  
+// var settings = {
+// 	success: function() {
+// 		movieCollection.forEach((todo) => {
+// 			console.log(todo.get('text'));
+// 			//instantiate each Todo Item View
+// 			let newPersonalTodoListView = new TodoItemView(
+// 				todo.get('text')
+// 			);
+// 			//append the 
+// 			$('.todo-list1').append(newPersonalTodoListView.el);
+// 		});
+// 	}
+// };
+// updatedMovieCollection.fetch(settings);
 
 
 
