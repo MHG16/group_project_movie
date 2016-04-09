c01a032fe30af140404791e372caccad44bacd08
Martin Grossmann <mhgrossmann@yahoo.com>
2016-04-09 18:10:42 -0500

    working on settings to display the top 4 movies

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -2,53 +2,40 @@
 import '../styles/main.scss';
 
 import $ from 'jquery';
-import Backbone from 'backbone'
+import Backbone from 'backbone';
 
-import MoviePageView from './views/MoviePageView'
+import MoviePageView from './views/MoviePageView';
 import movies from './collections/MovieCollection';
 
 
-let updatedMoviePage = new MoviePageView();
+const updatedMoviePage = new MoviePageView();
 
 $('body').html(updatedMoviePage.$el);
 
 
 //get the latest movie information from the server when page loads  
-var settings = {
+const settings = {
 	success: function() {
 		console.log(movies);
-		// movies.forEach((movie) => {
-		// 	console.log(movie.get('title'));
-		// 	console.log(movie.get('upvotes_count'));
-			
-		// //now use backbone sort function that takes a function(the comparitor) and the number of votes
-		// //then use backbone at to get the 0 through 3 elements in the array.  
+		// settings.each((movies) => {
+		// 	console.log(movies.get('title'));
+		// };
 
-		// sort_key: 'id',
-		// comparator: function (item) {
-		// 	return !item.get(this.sort_key);
-		// },
-
-		// sortByField: function (fieldName) {
-		// 	this.sort_key = fieldName;
-		// 	this.sort();
-		// },
-
-
-
-	
 
 		// //loop through the top four items in the array and display their titles
-		// // for (var i = 0; i < 4; i++) {
-		// // 	console.log(movie[i].upvotes_count));
-		// // 	$('.title').append(movie.get('title'));
-		// // };
-		
+		// for (var i = 0; i < movies.length; i++) {
+		// 	console.log(movies[i]);
+		// 	};
+		// };
+
+
 		
 
-		// });
-	}
+
+ }
+
 };
+
 movies.fetch(settings);
 
 
