ab70bf6afce70ced822a39b1390157b9a6dbc6dd
Martin Grossmann <mhgrossmann@yahoo.com>
2016-04-10 11:02:16 -0500

    made changes to GenreView, TopFourView

--- app/scripts/views/TopFourView.js
+++ app/scripts/views/TopFourView.js
@@ -5,7 +5,28 @@
 
 // const TopFourView = Backbone.View.extend ({
 
+// tagName: 'section',  //section is the root element 
 
+// 	initialize: function(title) {
+// 		console.log('initialize');
+// 		this.title = title;  
+// 		this.render();
+// 	},
+// 	render: function() {  //code to put the elements needed into the root element
+// 		console.log('render');
+// 		const template = `
+// 		<p>${this.title}</p>
+// 		`; 
+
+// 		//this.$el.html(this.title);  
+// 		this.$el.html(template);
+// 	}
+
+
+// 	// }
+
+	
+// });
 
 
 
