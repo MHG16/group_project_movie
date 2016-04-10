6e91859ec0821d565083267eb9ae0c095d453482
Martin Grossmann <mhgrossmann@yahoo.com>
2016-04-10 15:21:51 -0500

    several movies on page

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -10,19 +10,17 @@ import MovieCollection from './collections/MovieCollection';
 let movies = MovieCollection;
 
 
-
 var settings = {
 	success: function() {
 		
-
-		movies.forEach((movie) => {
-
-			//console.log(movie.get('title'));
+		MovieCollection.forEach((movie) => {
+		
 			let newMoviePageView = new MoviePageView(movie.get('title'), movie.get('genre'), movie.get('upvotes_count'));
 			$('body').append(newMoviePageView.el);
- 		});
-
+ 		});	
+	
 	}
+
 };
 
 movies.fetch(settings);
